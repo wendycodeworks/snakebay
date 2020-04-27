1121d3cf5182329bfdc2dd77945ed98a060b6ac2
wenzable <wendyuworks@gmail.com>
2020-04-27 17:46:44 +1000

    route listing pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ get "/", to: "pages#home", as: "root"
+
+ get "/listings", to: "listings#index", as: "listings"
+ post "/listings", to: "listings#create"
+ get "listings/new", to: "listings#new", as: "new_listing"
+ get "listings/:id", to: "listings#show", as: "listing"
+ put "/listings/:id", to: "listings#update"
+ patch "/listings/:id", to: "listings#update"
+ delete "listings/:id", to: "listings#destroy"
+ get "/listings/:id/edit", to: "listings#edit", as: "edit_listing"
+
+ get "*path", to: "pages#not_found"
 end
