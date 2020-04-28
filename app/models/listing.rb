64fe09a62326fcd5e9e7f7d13f3a0f329613fb2b
wenzable <wendyuworks@gmail.com>
2020-04-28 13:36:41 +1000

    create partial form

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,6 @@
 class Listing < ApplicationRecord
   belongs_to :breed
   enum sex: {female: 0, male: 1}
+  validates :title, :breed_id, :sex, :price, :deposit, :city, :state, :date_of_birth, presence: true
+  has_one_attached :picture
 end
