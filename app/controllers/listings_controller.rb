c444189611bbbf020bb3d621529ec3d376f33425
wenzable <wendyuworks@gmail.com>
2020-04-29 22:55:14 +1000

    add devise for user authentication

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,45 +1,71 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-
+ 
     def index
         @listings = Listing.all
     end
 
     def show
-      
     end
 
     def new
-      @listing = Listing.new
+        set_breeds_and_sexes
+        @listing = Listing.new
     end
 
     def create
-      @listing = Listing.create(listings_params)
-      if @listing.errors.any?
-        set_breeds_and_sexes
-        render "new"
-      else
-        redirect_to listings_paths
-      end
+        #finish logic for creating a record
+        @listing = Listing.create(listing_params)
+        if @listing.errors.any?
+            set_breeds_and_sexes
+            render "new"
+        else 
+            redirect_to listings_path
+        end
     end
 
     def edit
+        set_breeds_and_sexes
     end
 
     def update
-        Listing.update(listings_params)
-        redirect_to listings_paths
+        @listing = Listing.update(params["id"], listing_params)
+        if @listing.errors.any?
+            set_breeds_and_sexes
+            render "edit"
+        else 
+            redirect_to listings_path
+        end
     end
 
     def destroy
-        Listing.find(params["id"]).destroy
-        redirect_to listings_paths
+        Listing.find(params[:id]).destroy
+        redirect_to listings_path
     end
 
     private
 
     def set_listing
-    id = params[:id]
-    @listing = Listing.find(id)
+        @listing = Listing.find(params[:id])
     end
+
+    def set_user_listing
+        @listing = current_user.listings.find_by_id(params[:id])
+
+        if @listing == nil
+            redirect_to listings_path
+        end
+    end
+
+    def set_breeds_and_sexes
+        @breeds = Breed.all
+        @sexes = Listing.sexes.keys
+    end
+
+    def listing_params
+        params.require(:listing).permit(:title, :description, :breed_id, :sex, :city, :state, :price, :deposit, :date_of_birth, :diet, :picture)
+    end
+
+    
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,4 @@ Rails.application.routes.draw do
  delete "listings/:id", to: "listings#destroy"
  get "/listings/:id/edit", to: "listings#edit", as: "edit_listing"
 
- get "*path", to: "pages#not_found"
 end
